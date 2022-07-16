c4c215fc2eafb7b23b3266287731d290248652ef
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-07-16 15:21:12 +0300

    Update Machine_Learning_Steps.py

--- Machine_Learning_Steps.py
+++ Machine_Learning_Steps.py
@@ -4,26 +4,25 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# 1 - Load the data
+# 1 - Load the data || Split into train/test sets
 data = load_breast_cancer()
 
-# 2 - Instantiate the model || Split into train/test sets
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33)
 N, D = X_train.shape
 scaler = StandardScaler()
 x_train = scaler.fit_transform(X_train)
 x_test = scaler.fit_transform(X_test)
 
-# 3 - Build the model  
+# 2 - Build/Instantiate the model 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Input(shape=(D,)),
   tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# 4 - Fit the model
+# 3 - Fit the model
 r = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100)
 
-# 5 - Evaluate the model
+# 4 - Evaluate the model
 print('Train score: ', model.evaluate(X_train, y_train))
 print('Test score: ', model.evaluate(X_test, y_test))
